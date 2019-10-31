a826d8f6208a5348afd8601c58d3f598bba24af0
315920958@qq.com <315920958@qq.com>
2019-10-31 10:14:01 +0800

    index

---
+++ index.js
@@ -0,0 +1,11 @@
+const AppWithExpress =  require('./webservicecore/app/AppWithExpress');
+const ExceptoinWithErrorCode =  require('./webservicecore/app/ExceptoinWithErrorCode');
+const AppConfig =  require('./webservicecore/runapp/AppConfig');
+
+
+
+module.exports = {
+    AppWithExpress,
+    ExceptoinWithErrorCode,
+    AppConfig,
+}
