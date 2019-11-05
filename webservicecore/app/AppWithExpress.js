f6be62b4ee26d2098be3b498ecda90e5d454319f
315920958@qq.com <315920958@qq.com>
2019-11-05 18:47:34 +0800

    bug fix

--- webservicecore/app/AppWithExpress.js
+++ webservicecore/app/AppWithExpress.js
@@ -374,7 +374,8 @@ module.exports = class {
         var icoKeys = [ ...this.config.controllerIocKeys,...(obj.icoKeys || [])].map(this.formatKey) ;
         var configKeys = [  ...this.config.controllerAutoConfigKey,...(obj.autoConfigKey||[])].map(this.formatKey);
         icoKeys.forEach( ({sourceKey,destKey}) => obj[destKey] = !clear && DATA_POOL[sourceKey] || null );
-        icoKeys.forEach( ({sourceKey,destKey}) =>obj[destKey] = !clear && obj.context && obj.context[sourceKey] || null );
+        !clear && icoKeys.filter(({destKey}) =>!obj[destKey])
+            .forEach( ({sourceKey,destKey}) =>obj[destKey] = obj.context && obj.context[sourceKey] || null );
         configKeys.forEach( ({sourceKey,destKey}) => obj[destKey] = !clear && CONFIG_POOL[sourceKey] || null );
     }
 
